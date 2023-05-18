f4cf4c7c12ea91fa4ffca4b7bf579e712d091107
PakinDioxide <125773754+PakinDioxide@users.noreply.github.com>
2023-05-18 11:56:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ import torch
 # Load the model
 MODEL_URL = 'https://github.com/PakinDioxide/Dog-Breed-Classification/raw/main/models/dbc_resnet50_new_fastai.pkl'
 urllib.request.urlretrieve(MODEL_URL, "model.pkl")
-learn_inf = load_learner('model.pkl')
+learn_inf = load_learner('https://github.com/PakinDioxide/Dog-Breed-Classification/raw/main/models/dbc_resnet50_new_fastai.pkl')
