5881ec304134a0f10d56b6afc2c004d05786795e
PakinDioxide <125773754+PakinDioxide@users.noreply.github.com>
2023-05-18 11:55:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,40 +5,5 @@ import torch
 
 # Load the model
 MODEL_URL = 'https://github.com/PakinDioxide/Dog-Breed-Classification/raw/main/models/dbc_resnet50_new_fastai.pkl'
-path = Path('models')
-path.mkdir(parents=True, exist_ok=True)
-model_file = path/'dbc_resnet50_new_fastai.pkl'
-
-if not model_file.exists():
-    download_url(MODEL_URL, model_file)
-
-# Load the model using torch.load
-learn_inf = torch.load(model_file, map_location=torch.device('cpu'))
-learn_inf.dls.device = torch.device('cpu')
-
-# Sidebar
-st.sidebar.title('Enter a dog to classify')
-option = st.sidebar.radio('', ['Use a validation image', 'Use your own image'])
-
-if option == 'Use a validation image':
-    st.sidebar.title('Select a validation image')
-    valid_images = get_image_files('images/valid')
-    fname = st.sidebar.selectbox('', valid_images)
-else:
-    st.sidebar.title('Select an image to upload')
-    uploaded_file = st.sidebar.file_uploader('', type=['png', 'jpg', 'jpeg'])
-    if uploaded_file is not None:
-        img = PILImage.create(uploaded_file)
-    else:
-        valid_images = get_image_files('images/valid')
-        img = PILImage.create(valid_images[0])
-
-# Main page
-st.title('Dog Breed Classification')
-
-def predict(img):
-    pred, pred_idx, pred_prob = learn_inf.predict(img)
-    st.success(f"This is {pred} with a probability of {pred_prob[pred_idx]*100:.02f}%")
-    st.image(img.to_thumb(300, 300))
-
-predict(img)
+urllib.request.urlretrieve(MODEL_URL, "model.pkl")
+learn_inf = load_learner('model.pkl', cpu=True)
