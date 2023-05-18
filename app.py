a32552bb15c2ce02fdf634bdb809c8540dc1ad3e
PakinDioxide <125773754+PakinDioxide@users.noreply.github.com>
2023-05-18 12:13:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def predict(img, learn):
     st.image(img, use_column_width=True)
 
 # เปิดรูป
-img = Image.open(fname)
+# img = Image.open(fname)
 
-# เรียก function ทำนาย
-predict(img, learn_inf)
+# # เรียก function ทำนาย
+# predict(img, learn_inf)
