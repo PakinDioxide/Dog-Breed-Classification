a1e3d81b05e0158e5e3912b3862d41ca402aa3c1
PakinDioxide <125773754+PakinDioxide@users.noreply.github.com>
2023-05-18 10:43:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import urllib
+
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
     st.image(img)
+    
+MODEL_URL = 'https://github.com/PakinDioxide/Dog-Breed-Classification/blob/main/models/dbc_resnet50_new_fastai.pkl'
+urllib.request.urlretrieve(MODEL_URL, "model.pkl")
+learn_inf = load_learner('model.pkl', cpu=True)
