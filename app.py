28214b8de7863187b68186339142a68f6bc24252
PakinDioxide <125773754+PakinDioxide@users.noreply.github.com>
2023-12-08 21:38:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,130 +14,125 @@ import shutil
 #import streamlit มาในชื่อ st เพื่อใช้ในการสร้าง user interface
 import streamlit as st
 
-st.success(os.listdir())
+# clone github repository
+if not os.path.exists('/app/models/dbc_resnet50_new_fastai.pkl'):
+    for root, dirs, files in os.walk('/app'): #เคลียร์ folder
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
+    Repo.clone_from('https://github.com/PakinDioxide/Dog-Breed-Classification.git', '/app/')
 
-# # clone github repository
-# if not os.path.exists('/app/dog-breed-classification/Dog-Breed-Classification-Images/models/dbc_resnet50_new_fastai.pkl'):
-#     parent_dir = '/app/dog-breed-classification/Dog-Breed-Classification-Images'
-#     if not os.path.exists(parent_dir):
-#         os.makedirs(parent_dir)
-#     for root, dirs, files in os.walk('/app/dog-breed-classification/Dog-Breed-Classification-Images'): #เคลียร์ folder
-#         for f in files:
-#             os.unlink(os.path.join(root, f))
-#         for d in dirs:
-#             shutil.rmtree(os.path.join(root, d))
-#     Repo.clone_from('https://github.com/PakinDioxide/Dog-Breed-Classification.git', '/app/dog-breed-classification/Dog-Breed-Classification-Images')
-
-# # import pathlib
-# # temp = pathlib.PosixPath
-# # pathlib.PosixPath = pathlib.WindowsPath
+# import pathlib
+# temp = pathlib.PosixPath
+# pathlib.PosixPath = pathlib.WindowsPath
 
 
-# # MODEL_URL = "https://github.com/PakinDioxide/Dog-Breed-Classification/raw/main/models/dbc_resnet50_new_fastai.pkl"
-# # urllib.request.urlretrieve(MODEL_URL, "model.pkl")
-# learn_inf = load_learner('/app/dog-breed-classification/Dog-Breed-Classification-Images/models/dbc_resnet50_new_fastai.pkl', cpu=True)
+# MODEL_URL = "https://github.com/PakinDioxide/Dog-Breed-Classification/raw/main/models/dbc_resnet50_new_fastai.pkl"
+# urllib.request.urlretrieve(MODEL_URL, "model.pkl")
+learn_inf = load_learner('/app/models/dbc_resnet50_new_fastai.pkl', cpu=True)
 
-# # เราจะแบ่งหน้าจอเป็น 
-# # 1. sidebar ประกอบด้วยตัวเลือกรูปภาพ
-# # 2. main page ประกอบด้วยรูปและคำทำนาย
+# เราจะแบ่งหน้าจอเป็น 
+# 1. sidebar ประกอบด้วยตัวเลือกรูปภาพ
+# 2. main page ประกอบด้วยรูปและคำทำนาย
 
-# ##################################
-# # main page
-# ##################################
+##################################
+# main page
+##################################
 
-# # ใส่ title ของ main page
-# st.title("Dog Breed Classification")
-# # st.write(os.listdir('/app/dog-breed-classification/Dog-Breed-Classification-Images'))
+# ใส่ title ของ main page
+st.title("Dog Breed Classification")
+# st.write(os.listdir('/app/dog-breed-classification/Dog-Breed-Classification-Images'))
 
-# ##################################
-# # sidebar
-# ##################################
+##################################
+# sidebar
+##################################
 
-# #function การทำนาย
-# def predict(img, learn):
-#     # ย่อขนาดรูป
-#     pimg = img.resize([224,224])
+#function การทำนาย
+def predict(img, learn):
+    # ย่อขนาดรูป
+    pimg = img.resize([224,224])
     
-#     # ทำนายจากโมเดลที่ให้
-#     pred, pred_idx, pred_prob = learn.predict(pimg)
+    # ทำนายจากโมเดลที่ให้
+    pred, pred_idx, pred_prob = learn.predict(pimg)
         
-#     pred = pred.split('_')[1:]
+    pred = pred.split('_')[1:]
     
-#     if pred[-1] == 'Dog':
-#         pred = ' '.join(pred[:len(pred)-1])
-#     else:
-#         pred = ' '.join(pred)
+    if pred[-1] == 'Dog':
+        pred = ' '.join(pred[:len(pred)-1])
+    else:
+        pred = ' '.join(pred)
 
-#     # โชว์ผลการทำนาย
-#     st.success(f'This is "{pred} Dog" with the probability of {pred_prob[pred_idx]*100:.02f}%')
+    # โชว์ผลการทำนาย
+    st.success(f'This is "{pred} Dog" with the probability of {pred_prob[pred_idx]*100:.02f}%')
     
-#     # โชว์รูปที่ถูกทำนาย
-#     st.image(img, use_column_width=True)
+    # โชว์รูปที่ถูกทำนาย
+    st.image(img, use_column_width=True)
     
-#     st.balloons()
+    st.balloons()
 
-# # ใส่ title ของ sidebar
-# st.sidebar.write('# Upload a dog image to classify!')
+# ใส่ title ของ sidebar
+st.sidebar.write('# Upload a dog image to classify!')
 
-# # radio button สำหรับเลือกว่าจะทำนายรูปจาก validation set หรือ upload รูปเอง
-# option = st.sidebar.radio('', ['Use a validation image', 'Use your own image', 'Take a photo'])
-# # โหลดรูปจาก validation set แล้ว shuffle
-# valid_images = glob.glob('/app/dog-breed-classification/Dog-Breed-Classification-Images/images/test/*/*')
-# valid_images.sort()
-# for i in range(len(valid_images)):
-#     k = str(valid_images[i])
-#     k =k.replace('/app/dog-breed-classification/Dog-Breed-Classification-Images/images/test/', '')
-#     valid_images[i] = k
+# radio button สำหรับเลือกว่าจะทำนายรูปจาก validation set หรือ upload รูปเอง
+option = st.sidebar.radio('', ['Use a validation image', 'Use your own image', 'Take a photo'])
+# โหลดรูปจาก validation set แล้ว shuffle
+valid_images = glob.glob('/app/images/test/*/*')
+valid_images.sort()
+for i in range(len(valid_images)):
+    k = str(valid_images[i])
+    k =k.replace('/app/images/test/', '')
+    valid_images[i] = k
 
-# if option == 'Use a validation image':
-#     st.sidebar.write('### Select a validation image')
-#     fname = st.sidebar.selectbox('', valid_images)
+if option == 'Use a validation image':
+    st.sidebar.write('### Select a validation image')
+    fname = st.sidebar.selectbox('', valid_images)
     
-#     # เปิดรูป
-#     img = Image.open(f'/app/dog-breed-classification/Dog-Breed-Classification-Images/images/test/{fname}')
+    # เปิดรูป
+    img = Image.open(f'/app/images/test/{fname}')
 
-#     st.sidebar.image(img, f'Is this the image you want to predict?', use_column_width=True)
+    st.sidebar.image(img, f'Is this the image you want to predict?', use_column_width=True)
 
-#     if st.sidebar.button("Predict Now!"):
-#         # เรียก function ทำนาย
-#         predict(img, learn_inf)
+    if st.sidebar.button("Predict Now!"):
+        # เรียก function ทำนาย
+        predict(img, learn_inf)
         
-# elif option == 'Use your own image':
-#     st.sidebar.write('### Select an image to upload')
-#     fname = st.sidebar.file_uploader('',
-#                                      type=['jpg', 'jpeg', 'png'],
-#                                      accept_multiple_files=False)
-#     if fname is None:
-#         st.sidebar.write("Please select an image...")
-#     else:
-#         # เปิดรูป
-#         img = Image.open(fname)
-#         # เปลี่ยน format ภาพ
-#         img = img.convert('RGB')
-#         img.save('fname.jpg')
+elif option == 'Use your own image':
+    st.sidebar.write('### Select an image to upload')
+    fname = st.sidebar.file_uploader('',
+                                     type=['jpg', 'jpeg', 'png'],
+                                     accept_multiple_files=False)
+    if fname is None:
+        st.sidebar.write("Please select an image...")
+    else:
+        # เปิดรูป
+        img = Image.open(fname)
+        # เปลี่ยน format ภาพ
+        img = img.convert('RGB')
+        img.save('fname.jpg')
         
-#         img = Image.open('fname.jpg')
+        img = Image.open('fname.jpg')
         
-#         st.sidebar.image(img, f'Is this the image you want to predict?', use_column_width=True)
+        st.sidebar.image(img, f'Is this the image you want to predict?', use_column_width=True)
 
-#         if st.sidebar.button("Predict Now!"):
-#             # เรียก function ทำนาย
-#             predict(img, learn_inf)
-# else:
-#         fname = st.sidebar.camera_input('Take a photo of a dog')
-#         if fname is None:
-#             st.sidebar.write("Please take a photo...")
-#         else:
-#             # เปิดรูป
-#             img = Image.open(fname)
-#             # เปลี่ยน format ภาพ
-#             img = img.convert('RGB')
-#             img.save('fname.jpg')
+        if st.sidebar.button("Predict Now!"):
+            # เรียก function ทำนาย
+            predict(img, learn_inf)
+else:
+        fname = st.sidebar.camera_input('Take a photo of a dog')
+        if fname is None:
+            st.sidebar.write("Please take a photo...")
+        else:
+            # เปิดรูป
+            img = Image.open(fname)
+            # เปลี่ยน format ภาพ
+            img = img.convert('RGB')
+            img.save('fname.jpg')
 
-#             img = Image.open('fname.jpg')
+            img = Image.open('fname.jpg')
 
-#             st.sidebar.image(img, 'Is this the image you want to predict?', use_column_width=True)
+            st.sidebar.image(img, 'Is this the image you want to predict?', use_column_width=True)
 
-#             if st.sidebar.button("Predict Now!"):
-#                 # เรียก function ทำนาย
-#                 predict(img, learn_inf)
+            if st.sidebar.button("Predict Now!"):
+                # เรียก function ทำนาย
+                predict(img, learn_inf)
